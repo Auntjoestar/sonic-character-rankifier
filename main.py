b6c3a5e03a8244718fe39f159b7f4b85e763b82c
auntjoestar <88282065+Auntjoestar@users.noreply.github.com>
2026-02-01 15:03:03 -0600

    Add character overrated printer

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+def print_overrated_character(character: str):
+    return f"{character} is overrated!!!!"
+
+
 def main() -> None:
     print("Sonic es mejor que Freddy Fazbear")
-    
+
+
 if __name__ == "__main__":
     main()
+
