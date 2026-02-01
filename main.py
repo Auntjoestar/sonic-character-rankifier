17a76bcea99183e9c93b3651624734b344e9d713
auntjoestar <88282065+Auntjoestar@users.noreply.github.com>
2026-02-01 14:59:34 -0600

    Add character comparer

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+def rank_character(char1: str, char2: str) -> str:
+    return f"{char1} es mejor que {char2}"
+
+
 def main() -> None:
     print("Sonic es mejor que Freddy Fazbear")
-    
+
+
 if __name__ == "__main__":
     main()
